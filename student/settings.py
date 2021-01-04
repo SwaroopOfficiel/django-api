ac625d638f7485a1968c655c725841af7d56e9d3
Chaitanya <VankarChaitanya@outlook.com>
2021-01-04 22:47:38 +0530

    Update settings.py

--- student/settings.py
+++ student/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'studentdb',
         'USER' : 'postgres',
-        'PASSWORD' : '1234@Sonu',
+        'PASSWORD' : '',
         'HOST':'127.0.0.1',
         'PORT':'5432',
     }
